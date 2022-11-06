d9e9ff08446552e9a9560cbc67079c9c4b145179
jeremy-code254 <jeremyjey254@gmail.com>
2022-11-06 20:51:17 +0300

    initial commit

---
+++ _Android_homescreen/java/exportkit/figma/mask_group_activity.java
@@ -0,0 +1,53 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		homescreen
+	 *	@date 		1656659692218
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package exportkit.figma;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+
+public class mask_group_activity extends Activity {
+
+	
+	private View _bg__mask_group_ek2;
+	private View rectangle_1;
+	private ImageView add_image;
+	private ImageView upload;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mask_group);
+
+		
+		_bg__mask_group_ek2 = (View) findViewById(R.id._bg__mask_group_ek2);
+		rectangle_1 = (View) findViewById(R.id.rectangle_1);
+		add_image = (ImageView) findViewById(R.id.add_image);
+		upload = (ImageView) findViewById(R.id.upload);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
